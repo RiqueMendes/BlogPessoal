1ebe939fd34a9b70be54c118400084026a5ef990
RiqueMendes <97553942+RiqueMendes@users.noreply.github.com>
2022-03-29 15:58:03 -0300

    @RiqueMendes [task: prod]

--- src/main/java/org/generation/blogPessoal/seguranca/UserDetailsImpl.java
+++ src/main/java/org/generation/blogPessoal/seguranca/UserDetailsImpl.java
@@ -1,6 +1,7 @@
 package org.generation.blogPessoal.seguranca;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.generation.blogPessoal.model.Usuario;
 import org.springframework.security.core.GrantedAuthority;
@@ -12,6 +13,7 @@ public class UserDetailsImpl implements UserDetails{
 
     private String userName;
     private String password;
+    private List<GrantedAuthority> authorities;
 
     public UserDetailsImpl (Usuario user){
         this.userName = user.getUsuario();
@@ -22,8 +24,8 @@ public class UserDetailsImpl implements UserDetails{
 
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
     }
 
     @Override
